c946910d37eeb09c5287d7fc59edd0fb280e51a0
Júlio Oliveira <juliocesar.jc585@gmail.com>
2022-09-27 17:12:24 -0300

    #3SimuladorRedesSociais
    Projeto de simulador de uma rede social, o programa mostra na tela o comentário em ingles, relacionado a uma viagem e aparece alguns comentários agrupados de likes que esse post recebeu.

---
+++ SimladorRedesSociais/Entities/Post.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorRedesSociais.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        // Lista declarada devido a relação 1 para varios
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void AddRemove(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            // varias operações para se montar um texto a partir da propria função
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" LIKES - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Comments");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ SimladorRedesSociais/Program.cs
@@ -0,0 +1,25 @@
+﻿using SimuladorRedesSociais.Entities;
+using System;
+using System.Globalization;
+
+namespace SimuladorRedesSociais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("WOOOOW, that's awesome");
+            Post p1 = new Post(
+                DateTime.Parse("27/09/2022 17:00:45"),
+                "Traveling to Barcelona",
+                "I'm going to visit ths wonderful city!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
+        }
+
+    }
+}
