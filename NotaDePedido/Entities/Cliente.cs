fdee9e25fce6b8cfcbb22e1ac0ef09143e775258
Júlio Oliveira <juliocesar.jc585@gmail.com>
2022-09-28 16:29:05 -0300

    Nota_De_Pedidov1.0
    1º Versao do programa que simula uma nota de compra de um pedido, ao qual o usuário informa alguns dados relacionados a compra, tal como produto, quantidade , dentre outras informações .
        # Para essa versão, foi elaborado os códigos responsáveis pela composição das classes e sua respectiva enumeração de acordo com gráfico uml.

---
+++ NotaDePedido/Entities/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaPedidos.Entities
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+
+        // Construtor padrão
+        public Cliente()
+        {
+        }
+
+        public Cliente(string nome, string email, DateTime dataDeNascimento)
+        {
+            Nome = nome;
+            Email = email;
+            DataDeNascimento = dataDeNascimento;
+        }
+    }
+
+}
---
+++ NotaDePedido/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace NotaPedidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
