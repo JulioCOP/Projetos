2baf02abd3c714f8d021ee2580ba802041d4d578
Júlio Oliveira <juliocesar.jc585@gmail.com>
2022-09-29 16:37:25 -0300

    Atts_códigos_programa_classes_composições
    Adicionado códigos para programa principal  e para as classes correspondentes ao diagrama UML

---
+++ NotaDePedido/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaPedidos.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+       PagametnoPendente=0,
+       Processando=1,
+       Enviado =2,
+       Entregue =3
+    }
+}
---
+++ NotaDePedido/Entities/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaPedidos.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        // Contrutor padrão da classe
+        public Produto()
+        {
+        }
+
+        // Contrutor que rece a variavel como argumento
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+
+    }
+}
