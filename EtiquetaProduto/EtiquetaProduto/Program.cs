84e1d2589484f61a2df68a2509d81436d5d3ccf9
Júlio <juliocesar.jc585@gmail.com>
2022-10-27 07:28:22 -0300

    VERSÃO 1.3
    Corrigido o erro em caso do usuário informar alguma opção além das que foram solicitadas.
    Acrescentado condição while, para loop infinito, em caso do usuário selecione a opção errada.

--- EtiquetaProduto/EtiquetaProduto/Program.cs
+++ EtiquetaProduto/EtiquetaProduto/Program.cs
@@ -17,6 +17,17 @@ namespace EtiquetaProduto
                 Console.WriteLine($" DADOS DO PRODUTO #{i+1}");
                 Console.Write("Novo, usado ou importado ? [N] / [U] / [I] ");
                 char tipoProduto = char.Parse(Console.ReadLine());
+                while (tipoProduto != 'N' || tipoProduto!='n' || tipoProduto != 'U' || tipoProduto != 'u' || tipoProduto != 'I' || tipoProduto != 'i')
+                {
+                    Console.WriteLine("INFORME A OPÇÃO CORRETA");
+                    Console.WriteLine($" DADOS DO PRODUTO #{i + 1}");
+                    Console.Write("Novo, usado ou importado ? [N] / [U] / [I] ");
+                    tipoProduto = char.Parse(Console.ReadLine());
+                    if (tipoProduto == 'N' || tipoProduto == 'n' || tipoProduto == 'U' || tipoProduto == 'u' || tipoProduto == 'I' || tipoProduto == 'i') 
+                    { 
+                        break;
+                    }
+                }
                 Console.Write("NOME DO PRODUTO: ");
                 string nome = Console.ReadLine();
                 Console.Write("PREÇO DO PRODUTO: ");
@@ -39,10 +50,6 @@ namespace EtiquetaProduto
                     double custoDeTaxa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     list.Add(new ProdutoImportado(nome, preco, custoDeTaxa));
                 }
-                else
-                {
-                    Console.WriteLine("INFORME A OPÇÃO CORRETA");
-                }
             }
             Console.WriteLine();
             Console.WriteLine("ETIQUETA DE PREÇO");
@@ -54,12 +61,3 @@ namespace EtiquetaProduto
         }
     }
 }
-
-// DIA 24/10 - Logica inicial e estruturas inicial do programas 
-        // *** Conferir a herança das classes dos produtos importados e usados 
-// Dia 25/10 - Verificado todas as classes e consertado erro de herança das classes produto importado e produto usado
-           // -Adicionado comando virtual a função tagPreco na classe produto, para que assim a função possa ser herdada pelas outras classes
-        // *** Conferir lógica e continuar fazendo os códigos do programa principal que irá fazer a comunicação com as classes
-// Dia 26/10 - Adicionado comandos ao programa principal, para que o usúario possa informar os dados do produto a ser adquirido e o prpgrama lhe dê informações de acordo com sua classe e 
-                // informações passadas pelo usuário. Se é importado, usado ou novo.
-        // *** Verificar operadores - Função não retorna outro valor, quando informado uma opção além daquelas solicitadas pelo programa - Se o produto é novo, usado ou importado
