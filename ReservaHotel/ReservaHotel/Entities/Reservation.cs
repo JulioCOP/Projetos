22ef6005afe684f142f90b6d19a0a4a2fc6bdb91
Júlio <juliocesar.jc585@gmail.com>
2022-11-02 17:22:32 -0300

    ReservaHotel_V1.1
    programa para ler os dados de uma reserva de hotel (número do quarto, data
    de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
    dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
    novamente a reserva com os dados atualizados. O programa não deve aceitar dados
    inválidos para a reserva, conforme as seguintes regras: - Alterações de reserva só podem ocorrer para datas futuras
    - A data de saída deve ser maior que a data de entrada

---
+++ ReservaHotel/ReservaHotel/Entities/Reservation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReservaHotel.Entities.Exceptions;
+
+
+namespace ReservaHotel.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                // throw - substitui o return e além disso realiza o lançamento de um nova exceção
+                throw new DomainException("Check-out date must be after chek-in date");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }   
+        public double Duration()
+        {
+            // Duraç da reserva -  Fazer a diferença entre ambas as datas.
+
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            // TotalDays é uma função que retorna em double
+            // (int)Função -> castinh para que o comando retorne como inteiro
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                // throw - substitui o return e além disso realiza o lançamento de um nova exceção e corta toda método caso caia dentro do erro
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            else if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+
+        // Necessário faer o ToString da reserva ->  Impressão dos formatos de acordo com que é adotado como padrão
+        public override string ToString()
+        {
+            return "Room" +
+                RoomNumber +
+                " , check-in: "+
+                CheckIn.ToString("dd/MM/yyy") +
+                ", check-out: " +
+                CheckOut.ToString("dd/MM/yyy") +
+                " , " +
+                Duration() +
+                " nights";
+        }
+
+    }
+}
--- ReservaHotel/ReservaHotel/Program.cs
+++ ReservaHotel/ReservaHotel/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ReservaHotel.Entities;
+using ReservaHotel.Entities.Exceptions;
+using System;
+
+
+namespace ReservaHotel
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int numQuarto = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                // Instanciar a classe Reservetion
+                Reservation reservation = new Reservation(numQuarto, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+                Console.Write("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                // Chamada para atualização de datas
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation : " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+        }
+    }
+}
