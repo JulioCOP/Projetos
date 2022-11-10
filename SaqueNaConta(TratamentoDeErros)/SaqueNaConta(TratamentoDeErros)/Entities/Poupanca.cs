1f9035bb854ce3ee36cd92492fdd1aa6b21793b7
Júlio <juliocesar.jc585@gmail.com>
2022-11-09 21:40:11 -0300

    SaqueNaConta1.1
    //DIA 09/11/2022 -
             //             Adicionado comandos iniciais ao programa principal
             //             Alterado comando na entidade Poupança -  Implementação de superclasse
                                                              - Herança das classes da entidade e das execeções

--- SaqueNaConta(TratamentoDeErros)/SaqueNaConta(TratamentoDeErros)/Entities/Poupanca.cs
+++ SaqueNaConta(TratamentoDeErros)/SaqueNaConta(TratamentoDeErros)/Entities/Poupanca.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using SaqueNaConta_TratamentoDeErros_.Entities.Exceptions;
 
 namespace SaqueNaConta_TratamentoDeErros_.Entities
 {
-    internal class Poupanca
+    class Poupanca
     {
+        public int Numero { get; set; }
+        public string Usuario { get; set; }
+
+        public double Conta { get; set; }
+
+        public double SaqueLimite { get; set; } 
+
+        public Poupanca()
+        {
+
+        }
+        public Poupanca(int numero, string usuario, double conta, double saqueLimite)
+        {
+            Numero = numero;
+            Usuario = usuario;
+            Conta = conta;
+            SaqueLimite = saqueLimite;
+        }   
+        public void Deposito(double quantidade)
+        {
+            Conta += quantidade;
+        }
+        public void Saque(double quantidade)
+        {
+            if (quantidade> SaqueLimite)
+            {
+                throw new DomainException("A quantidade desejada excede o saque limite permitido"); 
+            }
+            if (quantidade> Conta)
+            {
+                throw new DomainException("Não possui saldo na conta");
+            }
+            Conta-=quantidade;
+        } 
     }
 }
--- SaqueNaConta(TratamentoDeErros)/SaqueNaConta(TratamentoDeErros)/Program.cs
+++ SaqueNaConta(TratamentoDeErros)/SaqueNaConta(TratamentoDeErros)/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+
+namespace SaqueNaConta_TratamentoDeErros_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DADOS DA CONTA BANCÁRIA");
+            Console.Write("NÚMERO: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("TITULAR DA CONTA: ");
+            string titular= Console.ReadLine();
+            Console.Write("SALDO INICIAL DA CONTA: ");
+            double contaInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("SAQUE LIMITE PERMITIDO: ");
+            double saqueLimite = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+         //DIA 09/11/2022 -
+         //             Adicionado comandos iniciais ao programa principal
+         //             Alterado comando na entidade Poupança -  Implementação de superclasse
+                                             //                 - Herança das classes da entidade e das execeções  
+        }
+    }
+}
