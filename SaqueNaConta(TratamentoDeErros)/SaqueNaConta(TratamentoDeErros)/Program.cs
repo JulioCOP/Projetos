1ef9bce5a6dff75ba7a326c84e9408ebf051498d
Júlio <juliocesar.jc585@gmail.com>
2022-11-16 07:52:55 -0300

    SaqueNaConta1.3
    // Dia 16/11 - Adicionado tratamento de erros (try/catch),através do instanciamento

--- SaqueNaConta(TratamentoDeErros)/SaqueNaConta(TratamentoDeErros)/Program.cs
+++ SaqueNaConta(TratamentoDeErros)/SaqueNaConta(TratamentoDeErros)/Program.cs
@@ -1,27 +1,40 @@
-﻿using System;
+﻿using SaqueNaConta_TratamentoDeErros_.Entities;
+using SaqueNaConta_TratamentoDeErros_.Entities.Exceptions;
+using System;
 using System.Diagnostics.Tracing;
 using System.Globalization;
-
 namespace SaqueNaConta_TratamentoDeErros_
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("DADOS DA CONTA BANCÁRIA");
             Console.Write("NÚMERO: ");
             int numero = int.Parse(Console.ReadLine());
             Console.Write("TITULAR DA CONTA: ");
-            string titular= Console.ReadLine();
+            string titular = Console.ReadLine();
             Console.Write("SALDO INICIAL DA CONTA: ");
             double contaInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("SAQUE LIMITE PERMITIDO: ");
             double saqueLimite = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Poupanca popanca = new Poupanca(numero, titular, contaInicial, saqueLimite);
+            
+            Console.WriteLine();
+            Console.Write("Informe a quantidade do saque: ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-         //DIA 09/11/2022 -
-         //             Adicionado comandos iniciais ao programa principal
-         //             Alterado comando na entidade Poupança -  Implementação de superclasse
-                                             //                 - Herança das classes da entidade e das execeções  
+            try
+            {
+                popanca.Saque(quantidade);
+                Console.WriteLine("Novo saldo: " + popanca.Conta.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("IMPOSSÍVEL REALIZAR SAQUE: " + e.Message);
+            }
         }
     }
 }
+ 
+
